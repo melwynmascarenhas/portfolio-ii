1a6791ae6f418ca4db0a60499d03b31dbd2868a4
Melwyn <121210177+melwynmascarenhas@users.noreply.github.com>
2025-01-12 19:06:02 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,8 +5,21 @@ import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { Flip } from 'gsap/Flip'
 import SplitType from 'split-type'
+import Lenis from 'lenis'
 
 gsap.registerPlugin(ScrollTrigger, Flip)
+
+//LENIS
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+})
+lenis.on('scroll', ScrollTrigger.update)
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000)
+})
+gsap.ticker.lagSmoothing(0)
+//
 
 //word masks description section
 function createAnimation() {
